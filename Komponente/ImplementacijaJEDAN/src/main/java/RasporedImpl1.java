ed931f3ec7e0a893c3281a9a08ebab61f9b53650
ilijastojmirovic <istojmirovic4521rn@raf.rs>
2023-10-29 19:29:48 +0100

    implementacija1

--- Komponente/ImplementacijaJEDAN/src/main/java/RasporedImpl1.java
+++ Komponente/ImplementacijaJEDAN/src/main/java/RasporedImpl1.java
@@ -1,5 +1,7 @@
 
 
+import lombok.Getter;
+import lombok.Setter;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVRecord;
@@ -8,9 +10,14 @@ import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
+@Getter
+@Setter
 public class RasporedImpl1 extends RasporedAC{
 
     private List<TerminImpl1> termini;
@@ -22,7 +29,9 @@ public class RasporedImpl1 extends RasporedAC{
 
     //Kolona sa prostorijom je poslednja kolona, vreme jedna pre nje, dan dve pre, datum tri pre nje
     @Override
-    public <T> T inicijalizacija(File file, String nazivRasporeda, LocalDate trajeOd, LocalDate trajeDo) {
+    public <T> T inicijalizacija(File file, String nazivRasporeda, LocalDate trajeOd, LocalDate trajeDo, List<Date> izuzetiDani) {
+
+
 
         BufferedReader reader = null;
         int brojKolona = 0;
@@ -45,18 +54,22 @@ public class RasporedImpl1 extends RasporedAC{
                 }
                 dodajProstoriju(new Prostorija(null,csvRecord.get(brojKolona-1)));
             }
+            System.out.println(this.getKolone());
+            System.out.println(this.getProstorije());
             reader.close();
             reader = new BufferedReader(new FileReader(file));
             csvParser = new CSVParser(reader, CSVFormat.DEFAULT);
             prviProlazak = 0;
             for(CSVRecord csvRecord : csvParser){
-                if(prviProlazak == 0)
+                if(prviProlazak == 0){
+                    prviProlazak++;
                     continue;
+                }
                 List<String> zaTermin = new ArrayList<>();
                 for(int i=0; i<brojKolona; i++){
                     zaTermin.add(csvRecord.get(i));
                 }
-                dodajNovTermin(zaTermin);
+                dodajNovTermin(zaTermin, true);
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -73,13 +86,20 @@ public class RasporedImpl1 extends RasporedAC{
     }
 
     @Override
-    public <T> T inicijalizacija(List<String> kolone, String nazivRasporeda, LocalDate trajeOd, LocalDate trajeDo) {
+    public <T> T inicijalizacija(List<String> kolone, String nazivRasporeda, LocalDate trajeOd, LocalDate trajeDo, List<Date> izuzetiDani) {
+        this.setNazivRasporeda(nazivRasporeda);
+        this.setTrajeOd(trajeOd);
+        this.setTrajeDo(trajeDo);
+        this.setIzuzetiDani(izuzetiDani);
+        dodajNovTermin(kolone, true);
         return null;
     }
 
 
     @Override
-    public <T> T dodajNovTermin(List<String> termin) {
+    public <T> T dodajNovTermin(List<String> termin, Boolean oznacenDatum) {
+
+        //ZAVRSI PROVERE!!!!!!!!!!!!
         int brKolona = termin.size();
         boolean ima = false;
         for(Prostorija p : this.getProstorije()){
@@ -92,17 +112,47 @@ public class RasporedImpl1 extends RasporedAC{
             for(int i=0; i < brKolona - 4; i++){
                 zaOstalo.add(termin.get(i));
             }
-            //brKolona -= 4;
 
-            //Treba da zavrsim za vreme,dan,datum...
+            // 11:00-13:00
+            List<String> vreme = parsirajVreme(termin.get(brKolona-2));
+            String dan = termin.get(brKolona-3);
+            String dateString = termin.get(brKolona-4);
 
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+            LocalDate date = LocalDate.parse(dateString, formatter);
 
-            //Termin novTermi = new TerminImpl1();
+            Termin termin1 = new TerminImpl1(LocalTime.parse(vreme.get(0)),LocalTime.parse(vreme.get(1)),new Prostorija(null,termin.get(brKolona-1)));
+            termin1.setOstalo(zaOstalo);
+            termin1.setDan(dan);
+            ((TerminImpl1)termin1).setDatum(date);
+            this.termini.add((TerminImpl1) termin1);
+
+            //jos datum
         } else {
             System.out.println("Ne postoji ovakva prostorija");
             return null; //moglo bi da se napravi neki exception nesto za ovo
         }
         return null;
+    }
+
+    public List<String> parsirajVreme(String vreme){
+        List<String> parsirano = new ArrayList<>();
+        String[] delovi = vreme.split("-");
+
+        for(String deo : delovi){
+            if(deo.contains(":")){
+                if(deo.length() == 4)
+                    parsirano.add("0" + deo);
+                else
+                    parsirano.add(deo);
+            } else {
+                if(deo.length() == 1)
+                    parsirano.add("0" + deo + ":00");
+                else
+                    parsirano.add(deo + ":00");
+            }
+        }
+        return  parsirano;
     }
 
     @Override
--- Komponente/ImplementacijaJEDAN/src/main/java/TerminImpl1.java
+++ Komponente/ImplementacijaJEDAN/src/main/java/TerminImpl1.java
@@ -2,7 +2,9 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.Date;
+import java.util.List;
 
 
 @Getter
@@ -11,7 +13,22 @@ public class TerminImpl1 extends Termin{
 
     private LocalDate datum;
 
-    public TerminImpl1(Date vreme, Prostorija mesto) {
-        super(vreme, mesto);
+
+    public TerminImpl1(LocalTime pocetak, LocalTime kraj, Prostorija mesto) {
+        super(pocetak, kraj, mesto);
+    }
+
+
+    @Override
+    public String toString() {
+        return "Termin: " + ostalo(getOstalo()) + " | " + this.getSatPocetka() + "-" + this.getSatKraja() + " | " + getDatum() + " | " + getDan() + " | "  + getMesto() + "|\n";
+    }
+
+    private String ostalo(List<String> ostalo){
+        StringBuilder sb = new StringBuilder();
+        for(int i=0;i<ostalo.size()-1;i++)
+            sb.append(ostalo.get(i) + " ");
+        sb.append(ostalo.get(ostalo.size()-1));
+        return sb.toString();
     }
 }
