3a19d80890c1a926219157530206c38acade5023
ilijastojmirovic <istojmirovic4521rn@raf.rs>
2023-11-03 20:31:20 +0100

    Nema JSON

--- Komponente/ImplementacijaJEDAN/src/main/java/Main.java
+++ Komponente/ImplementacijaJEDAN/src/main/java/Main.java
@@ -3,7 +3,9 @@ import java.io.FileReader;
 import java.io.InputStream;
 import java.net.URL;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
@@ -13,13 +15,26 @@ public class Main {
         File file = new File("C:\\Users\\I L I J A\\Desktop\\Komponente-1\\raspored1.csv");
         LocalDate pocetak = null;
         LocalDate kraj = null;
+        //File file2 = new File("C:\\Users\\I L I J A\\Desktop\\json1.json");
 
         raspored.inicijalizacija("aaa",pocetak,kraj,null);
         raspored.CSVread(file);
+        //raspored.JSONread(file2);
 
         //System.out.println(raspored.getKolone());
         //System.out.println(raspored.getProstorije());
 
-        System.out.println(((RasporedImpl1)raspored).getTermini());
+        System.out.println(raspored.getTermini());
+        /*System.out.println("-------------------");
+        List<String> novTermin = new ArrayList<>();
+        novTermin.add("zzz");
+        novTermin.add("aaa");
+        novTermin.add("22-05-2000");
+        novTermin.add("CET");
+        novTermin.add("15:00-17");
+        novTermin.add("Med");
+        //raspored.dodajProstoriju("Ucionica neka");
+        raspored.dodajNovTermin(novTermin,true);
+        System.out.println(raspored.getTermini());*/
     }
 }
